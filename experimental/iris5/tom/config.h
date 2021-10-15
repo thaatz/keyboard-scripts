1919c6ced4dedf6b1e1cf136fcb446ed475a20bf
Tom Hongsmatip <tom.hongsmatip@gmail.com>
2021-10-14 21:51:22 -0400

    added personal irisv5 firmware source

---
+++ experimental/iris5/tom/config.h
@@ -0,0 +1,17 @@
+// https://www.reddit.com/r/olkb/comments/72u8ou/qmk_mouse_keys_rock/
+// https://github.com/qmk/qmk_firmware/blob/master/keyboards/ergodox_ez/config.h
+
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+// #define MOUSEKEY_TIME_TO_MAX 60
+// #define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_TIME_TO_MAX 40
+#define MOUSEKEY_MAX_SPEED 5
+#define MOUSEKEY_WHEEL_DELAY 0
+
+// https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold
+#define TAPPING_TERM 150
+// https://precondition.github.io/home-row-mods
+// https://docs.qmk.fm/#/config_options?id=behaviors-that-can-be-configured
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
---
+++ experimental/iris5/tom/rules.mk
@@ -0,0 +1,5 @@
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+ENCODER_ENABLE = no
+# TAP_DANCE_ENABLE = yes
+MOUSEKEY_ENABLE = yes        # Mouse keys
